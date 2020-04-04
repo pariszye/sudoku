9e591ffd079b16c9601000fde69ccb7b8dc983d9
pariszye <63172426+pariszye@users.noreply.github.com>
2020-04-04 19:19:45 -0400

    adding sudoku javascript file

---
+++ script.js
@@ -0,0 +1,346 @@
+// START OF Emanuele Feronato's CODE
+// we start with an empty sudoku...
+var sudoku = new Array(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+
+let newSudoku;
+
+// ... and we solve it!!
+solve(sudoku);
+
+// given a sudoku cell, returns the row
+function returnRow(cell) {
+	return Math.floor(cell / 9);
+}
+
+// given a sudoku cell, returns the column
+function returnCol(cell) {
+	return cell % 9;
+}
+
+// given a sudoku cell, returns the 3x3 block
+function returnBlock(cell) {
+	return Math.floor(returnRow(cell) / 3) * 3 + Math.floor(returnCol(cell) / 3);
+}
+
+// given a number, a row and a sudoku, returns true if the number can be placed in the row
+function isPossibleRow(number,row,sudoku) {
+	for (var i=0; i<=8; i++) {
+		if (sudoku[row*9+i] == number) {
+			return false;
+		}
+	}
+	return true;
+}
+
+// given a number, a column and a sudoku, returns true if the number can be placed in the column
+function isPossibleCol(number,col,sudoku) {
+	for (var i=0; i<=8; i++) {
+		if (sudoku[col+9*i] == number) {
+			return false;
+		}
+	}
+	return true;
+}
+
+// given a number, a 3x3 block and a sudoku, returns true if the number can be placed in the block
+function isPossibleBlock(number,block,sudoku) {
+	for (var i=0; i<=8; i++) {
+		if (sudoku[Math.floor(block/3)*27+i%3+9*Math.floor(i/3)+3*(block%3)] == number) {
+			return false;
+		}
+	}
+	return true;
+}
+
+// given a cell, a number and a sudoku, returns true if the number can be placed in the cell
+function isPossibleNumber(cell,number,sudoku) {
+	var row = returnRow(cell);
+	var col = returnCol(cell);
+	var block = returnBlock(cell);
+	return isPossibleRow(number,row,sudoku) && isPossibleCol(number,col,sudoku) && isPossibleBlock(number,block,sudoku);
+}
+
+// given a row and a sudoku, returns true if it's a legal row
+function isCorrectRow(row,sudoku) {
+	var rightSequence = new Array(1,2,3,4,5,6,7,8,9);
+	var rowTemp= new Array();
+	for (var i=0; i<=8; i++) {
+		rowTemp[i] = sudoku[row*9+i];
+	}
+	rowTemp.sort();
+	return rowTemp.join() == rightSequence.join();
+}
+
+// given a column and a sudoku, returns true if it's a legal column
+function isCorrectCol(col,sudoku) {
+	var rightSequence = new Array(1,2,3,4,5,6,7,8,9);
+	var colTemp= new Array();
+	for (var i=0; i<=8; i++) {
+		colTemp[i] = sudoku[col+i*9];
+	}
+	colTemp.sort();
+	return colTemp.join() == rightSequence.join();
+}
+
+// given a 3x3 block and a sudoku, returns true if it's a legal block 
+function isCorrectBlock(block,sudoku) {
+	var rightSequence = new Array(1,2,3,4,5,6,7,8,9);
+	var blockTemp= new Array();
+	for (var i=0; i<=8; i++) {
+		blockTemp[i] = sudoku[Math.floor(block/3)*27+i%3+9*Math.floor(i/3)+3*(block%3)];
+	}
+	blockTemp.sort();
+	return blockTemp.join() == rightSequence.join();
+}
+
+// given a sudoku, returns true if the sudoku is solved
+function isSolvedSudoku(sudoku) {
+	for (var i=0; i<=8; i++) {
+		if (!isCorrectBlock(i,sudoku) || !isCorrectRow(i,sudoku) || !isCorrectCol(i,sudoku)) {
+			return false;
+		}
+	}
+	return true;
+}
+
+// given a cell and a sudoku, returns an array with all possible values we can write in the cell
+function determinePossibleValues(cell,sudoku) {
+	var possible = new Array();
+	for (var i=1; i<=9; i++) {
+		if (isPossibleNumber(cell,i,sudoku)) {
+			possible.unshift(i);
+		}
+	}
+	return possible;
+}
+
+// given an array of possible values assignable to a cell, returns a random value picked from the array
+function determineRandomPossibleValue(possible,cell) {
+	var randomPicked = Math.floor(Math.random() * possible[cell].length);
+	return possible[cell][randomPicked];
+}
+
+// given a sudoku, returns a two dimension array with all possible values 
+function scanSudokuForUnique(sudoku) {
+	var possible = new Array();
+	for (var i=0; i<=80; i++) {
+		if (sudoku[i] == 0) {
+			possible[i] = new Array();
+			possible[i] = determinePossibleValues(i,sudoku);
+			if (possible[i].length==0) {
+				return false;
+			}
+		}
+	}
+	return possible;
+}
+
+// given an array and a number, removes the number from the array
+function removeAttempt(attemptArray,number) {
+	var newArray = new Array();
+	for (var i=0; i<attemptArray.length; i++) {
+		if (attemptArray[i] != number) {
+			newArray.unshift(attemptArray[i]);
+		}
+	}
+	return newArray;
+}
+
+// given a two dimension array of possible values, returns the index of a cell where there are the less possible numbers to choose from
+function nextRandom(possible) {
+	var max = 9;
+	var minChoices = 0;
+	for (var i=0; i<=80; i++) {
+		if (possible[i]!=undefined) {
+			if ((possible[i].length<=max) && (possible[i].length>0)) {
+				max = possible[i].length;
+				minChoices = i;
+			}
+		}
+	}
+	return minChoices;
+}
+
+
+// given a sudoku, solves it
+function solve(sudoku) {
+	var saved = new Array();
+	var savedSudoku = new Array();
+	var i=0;
+	var nextMove;
+	var whatToTry;
+	var attempt;
+	while (!isSolvedSudoku(sudoku)) {
+		i++;
+		nextMove = scanSudokuForUnique(sudoku);
+		if (nextMove == false) {
+			nextMove = saved.pop();
+			sudoku = savedSudoku.pop();
+		}
+		whatToTry = nextRandom(nextMove);
+		attempt = determineRandomPossibleValue(nextMove,whatToTry);
+		if (nextMove[whatToTry].length>1) {
+			nextMove[whatToTry] = removeAttempt(nextMove[whatToTry],attempt);
+			saved.push(nextMove.slice());
+			savedSudoku.push(sudoku.slice());
+		}
+		sudoku[whatToTry] = attempt;
+	}
+  newSudoku = sudoku;
+}
+
+
+
+// END OF Emanuele Feronato's CODE
+
+
+
+
+
+// Defining global variables of the 3 boards
+let totalBoardSolution = newSudoku;
+console.log(totalBoardSolution);
+let emptyBoard = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+let revealedBoard = [];
+
+// Choosing and displaying the revealed numbers on the board 
+for (let i = 0; i < totalBoardSolution.length; i++) {
+  let showNumber = Math.round(Math.random());
+  if (showNumber == 1) {
+    document.getElementById(i).innerHTML = totalBoardSolution[i];
+    revealedBoard.push(1);
+  } else {
+    revealedBoard.push(0);
+  }
+}
+console.log(revealedBoard);
+
+
+// PLAYING CODE
+
+
+
+// CLICKING THE NUMERAL BUTTONS
+	// this determines what number is currently being used 
+	let currentOperation = 1;
+	// this gets all the buttons
+	const numButtons = document.querySelectorAll('.numerals');
+	 numButtons[0].addEventListener('click', event => {
+	  currentOperation = event.target.id;
+		console.log(currentOperation);
+	});
+	 numButtons[1].addEventListener('click', event => {
+	  currentOperation = event.target.id;
+		console.log(currentOperation);
+	});
+	 numButtons[2].addEventListener('click', event => {
+	  currentOperation = event.target.id;
+		console.log(currentOperation);
+	});
+	 numButtons[3].addEventListener('click', event => {
+	  currentOperation = event.target.id;
+		console.log(currentOperation);
+	});
+	 numButtons[4].addEventListener('click', event => {
+	  currentOperation = event.target.id;
+		console.log(currentOperation);
+	});
+	 numButtons[5].addEventListener('click', event => {
+	  currentOperation = event.target.id;
+		console.log(currentOperation);
+	});
+	 numButtons[6].addEventListener('click', event => {
+	  currentOperation = event.target.id;
+		console.log(currentOperation);
+	});
+	 numButtons[7].addEventListener('click', event => {
+	  currentOperation = event.target.id;
+		console.log(currentOperation);
+	});
+	 numButtons[8].addEventListener('click', event => {
+	  currentOperation = event.target.id;
+		console.log(currentOperation);
+	});
+
+// CLICKING THE ERASER/PENCIL BUTTONS
+const writingButtons = document.querySelectorAll('.writing');
+let currentUtensil = 0;
+ writingButtons[0].addEventListener('click', event => {
+  currentUtensil = event.target.id;
+	console.log(currentUtensil);
+});
+ writingButtons[1].addEventListener('click', event => {
+  currentUtensil = event.target.id;
+	console.log(currentUtensil);
+});
+
+
+// this updates the box depending on which number is clicked: first determines if the box is revealed, then updates depending on if the pen or eraser is selected
+function updateBoxClicked(boxNumber) {
+	  if (revealedBoard[boxNumber] == 0) {
+      if (currentUtensil == 0)  {
+        emptyBoard[boxNumber] = currentOperation;
+				emptyBoard = emptyBoard.map(Number);
+				console.log(emptyBoard);
+				document.getElementById(boxNumber).innerHTML = currentOperation;
+				document.getElementById(boxNumber).style.backgroundColor = 'white';
+     } else {
+				document.getElementById(boxNumber).innerHTML = "";
+  }
+ }
+}
+//WHEN CHECK IS CLICKED.... 
+
+let clickCheck = document.querySelector('#check');
+
+clickCheck.addEventListener('click', event => {
+	checking();
+})
+
+function checking() {
+	// CHECKING (0 is right, 1 is wrong)
+	let checkCorrect = [];
+	const emptyCheckCorrect = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+	console.log(emptyBoard);
+	console.log(totalBoardSolution);
+	for (let i = 0; i < 81; i++) {
+	    if (revealedBoard[i] == 0) {
+				if (emptyBoard[i] !== totalBoardSolution[i]) {
+					checkCorrect.push(1);
+					document.getElementById(i).style.backgroundColor = 'red';
+				} else {
+							checkCorrect.push(0);
+				}
+	    } else { 
+				checkCorrect.push(0);
+			}
+	}
+	if (checkCorrect == emptyCheckCorrect) {
+
+		console.log('YAY good job');
+	} else {
+		console.log('oopsie, look at the red');
+	}
+}
+
+
+/* let cell = document.getElementsByTagName("td");
+		cell[i].addEventListener('click', function() {
+        updateBoxClicked(this.id);
+				console.log(this.id);
+    });
+	var x = document.getElementsByTagName("P");
+for (let i = 0; i < x.length; i++) {
+  x[i].style.backgroundColor = "red";
+}	 */
+
+let cell = document.getElementsByTagName("td");
+const cellCount = cell.length;
+for (let i = 0; i <= cellCount; i += 1) {
+	let cellReplacement = cell[i];	
+	cellReplacement.addEventListener('click', function() {
+        updateBoxClicked(this.id);
+				console.log(this.id);
+    });
+}
+
